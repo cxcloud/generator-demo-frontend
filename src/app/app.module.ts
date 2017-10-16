c073d2438074a110126b5eb47f6838bb93010e39
Nadia Kolodyazhnaya <nadezda.koodyazhnaya@tieto.com>
2017-10-16 22:08:37 +0300

    Added variables.scss; Added footer placehoder;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BrandNavbarComponent } from './header/brand-navbar/brand-navbar.component';
 import { MainMenuComponent } from './header/main-menu/main-menu.component';
+import { FooterComponent } from './footer/footer.component';
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     BrandNavbarComponent,
-    MainMenuComponent
+    MainMenuComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
