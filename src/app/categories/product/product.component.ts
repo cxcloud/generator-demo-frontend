9698a251f50ff220d0d5163ac70a1add97ca6cd5
Nadia Kolodyazhnaya <nadia.kolodyazhnaya@gmail.com>
2017-10-31 10:45:49 +0200

    Updated getVariantByAttribute and getAvailableVariantOptions considering multiple nested object values;

--- src/app/categories/product/product.component.ts
+++ src/app/categories/product/product.component.ts
@@ -3,6 +3,7 @@ import { CommerceService } from '../../core/commerce/commerce.service';
 import { ActivatedRoute } from '@angular/router';
 import { Product, Variant } from '../../types/product.model';
 import { Image, Price } from '../../types/common.model';
+
 @Component({
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss'],
@@ -11,7 +12,7 @@ import { Image, Price } from '../../types/common.model';
 export class ProductComponent implements OnInit {
   product: Product;
   variants: Variant[];
-  variant: Variant;
+  selectedVariant: Variant;
   image: Image;
   price: Price;
   colors: Array<string>;
@@ -35,64 +36,83 @@ export class ProductComponent implements OnInit {
     .subscribe(product => {
       if (product) {
         this.product = product;
-        console.log(product);
         this.setDefaultVariant(product.masterVariant);
+
+        // Get all variants (including master variant)
         this.variants = this.getAllVariants(product);
 
         // Get available colors
-        this.colors = this.getAvailableOptions('color');
+        this.colors = this.getAvailableVariantOptions('color');
 
         // Get available sizes
-        this.sizes = this.getAvailableOptions('size');
+        this.sizes = this.getAvailableVariantOptions('size');
       }
     });
   }
 
   setDefaultVariant(variant) {
-    this.variant = variant;
+    this.selectedVariant = variant;
     this.image = variant.images[0];
     this.price = variant.prices[0];
   }
 
-  getAvailableOptions(attributeName: string) {
+  getAllVariants(product) {
+    return product.variants.reduce((acc: Variant[], variant: Variant) => {
+     acc.push(variant);
+     return acc;
+    }, [product.masterVariant]);
+   }
+
+  getAvailableVariantOptions(attributeName: string) {
+
+    const isOptionAvailable = function(array, property) {
+      if (typeof property !== 'object') {
+        if (array.indexOf(property) === -1) {
+          array.push(property);
+          return array;
+        }
+      } else {
+        Object.keys(property).forEach(key => {
+          isOptionAvailable(array, property[key]);
+        });
+      }
+    };
+
     return this.variants.reduce((acc, variant) => {
       variant.attributes.forEach(attr => {
-
-        if (attributeName === 'color' && attr.name === attributeName) {
-
-          if (acc.indexOf(attr.value.label.en) === -1) {
-            acc.push(attr.value.label.en);
-          }
-        } else if (attributeName === 'size' && attr.name === attributeName) {
-          if (acc.indexOf(attr.value) === -1) {
-            acc.push(attr.value);
-          }
+        if (attr.name === attributeName) {
+          isOptionAvailable(acc, attr.value);
         }
       });
       return acc;
     }, []);
   }
 
-  getAllVariants(product) {
-   return product.variants.reduce((acc: Variant[], variant: Variant) => {
-    acc.push(variant);
-    return acc;
-   }, [product.masterVariant]);
-  }
 
   selectImage(image) {
     this.image = image;
   }
 
-  getVariantByAttribute(attrName, event) {
-    const selectedVariant = this.variants.filter(variant => {
-      for (let i = 0; i < variant.attributes.length; i += 1) {
-        if (variant.attributes[i].name === attrName &&
-          variant.attributes[i].value === event.target.value) {
+  getVariantByAttribute(attrName, $event) {
+
+    const deepCheck = function(property, value) {
+      if (typeof property !== 'object') {
+        if (property === value) {
           return true;
+        }
+      } else {
+        Object.keys(property).forEach(key => {
+          deepCheck(property[key], value);
+        });
+      }
+    };
+
+    this.selectedVariant = this.variants.filter(variant => {
+      for (let i = 0; i < variant.attributes.length; i += 1) {
+        if (variant.attributes[i].name === attrName) {
+          return deepCheck(variant.attributes[i].value, $event.target.value);
         }
       }
     })[0];
-    this.variant = selectedVariant;
   }
 }
