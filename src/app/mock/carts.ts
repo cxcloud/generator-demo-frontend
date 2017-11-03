8218903d787b398d78c1013b735384d31d75c98c
Nadia Kolodyazhnaya <nadia.kolodyazhnaya@gmail.com>
2017-11-03 09:30:37 +0200

    Added extra mock lineItem to the cart; Removed unecessary td borders;

--- src/app/mock/carts.ts
+++ src/app/mock/carts.ts
@@ -198,6 +198,198 @@ export const CART = {
           'centAmount': 76875
       },
       'lineItemMode': 'Standard'
+    },
+    {
+      'id': '834433eb-eb38-424b-85ac-2eb40b939c61',
+      'productId': 'bebd3f35-4c7e-4629-bbee-d6e53cbe1cb0',
+      'name': {
+          'de': 'Tasche Moschino Love rot',
+          'en': 'Bag Moschino Love red'
+      },
+      'productType': {
+          'typeId': 'product-type',
+          'id': '021022d8-3d52-476f-9ead-39953eb59a2f',
+          'version': 1
+      },
+      'productSlug': {
+          'en': 'moschinolove-bag-jc4057pp1kle090a-red',
+          'de': 'moschinolove-tasche-jc4057pp1kle090a-rot'
+      },
+      'variant': {
+          'id': 1,
+          'sku': 'A0E2000000021V5',
+          'prices': [
+            {
+                'value': {
+                  'currencyCode': 'EUR',
+                  'centAmount': 25625
+                },
+                'id': '4521473e-afdf-44e8-8147-337f855af4cf'
+            },
+            {
+                'value': {
+                  'currencyCode': 'EUR',
+                  'centAmount': 16803
+                },
+                'id': 'dbd6b2c8-f539-4812-9c17-4bbc4ab2f7b8',
+                'customerGroup': {
+                  'typeId': 'customer-group',
+                  'id': '7bd113cf-e862-48df-9cbd-c46dea2dfbab'
+                }
+            },
+            {
+                'value': {
+                  'currencyCode': 'EUR',
+                  'centAmount': 20500
+                },
+                'id': 'd5eeef12-54f5-40ca-9646-2b5d48cbde15',
+                'country': 'DE'
+            },
+            {
+                'value': {
+                  'currencyCode': 'EUR',
+                  'centAmount': 20500
+                },
+                'id': 'a4189a55-7aa6-4882-971a-23b2d03322c8',
+                'country': 'IT'
+            },
+            {
+                'value': {
+                  'currencyCode': 'EUR',
+                  'centAmount': 20500
+                },
+                'id': 'ee632332-ee53-432e-8943-16708af058b8',
+                'country': 'GB'
+            }
+          ],
+          'images': [
+            {
+              'url': 'https://s3-eu-west-1.amazonaws.com/commercetools-maximilian/products/079375_1_large.jpg',
+              'dimensions': {
+                'w': 0,
+                'h': 0
+              }
+            }
+          ],
+          'attributes': [
+            {
+                'name': 'articleNumberManufacturer',
+                'value': 'JC4057PP1KLE090A TWINS'
+            },
+            {
+                'name': 'articleNumberMax',
+                'value': '79375'
+            },
+            {
+                'name': 'matrixId',
+                'value': 'A0E2000000021V5'
+            },
+            {
+                'name': 'baseId',
+                'value': '79375'
+            },
+            {
+                'name': 'designer',
+                'value': {
+                  'key': 'moschinolove',
+                  'label': 'Moschino Love'
+                }
+            },
+            {
+                'name': 'madeInItaly',
+                'value': {
+                  'key': 'no',
+                  'label': 'no'
+                }
+            },
+            {
+                'name': 'completeTheLook',
+                'value': [
+                  ''
+                ]
+            },
+            {
+                'name': 'commonSize',
+                'value': {
+                  'key': 'oneSize',
+                  'label': 'one Size'
+                }
+            },
+            {
+                'name': 'size',
+                'value': 'one size'
+            },
+            {
+                'name': 'color',
+                'value': {
+                  'key': 'red',
+                  'label': {
+                      'de': 'rot',
+                      'en': 'red',
+                      'it': 'rosso'
+                  }
+                }
+            },
+            {
+                'name': 'colorFreeDefinition',
+                'value': {
+                  'de': 'rot',
+                  'en': 'red'
+                }
+            },
+            {
+                'name': 'style',
+                'value': {
+                  'key': 'sporty',
+                  'label': 'sporty'
+                }
+            },
+            {
+                'name': 'gender',
+                'value': {
+                  'key': 'women',
+                  'label': 'Damen'
+                }
+            },
+            {
+                'name': 'season',
+                'value': 's15'
+            },
+            {
+                'name': 'lookProducts',
+                'value': [
+                  ''
+                ]
+            }
+          ],
+          'assets': [
+
+          ]
+      },
+      'price': {
+          'value': {
+            'currencyCode': 'EUR',
+            'centAmount': 25625
+          },
+          'id': '4521473e-afdf-44e8-8147-337f855af4cf'
+      },
+      'quantity': 3,
+      'discountedPricePerQuantity': [],
+      'state': [
+          {
+            'quantity': 3,
+            'state': {
+                'typeId': 'state',
+                'id': '7331f2be-5864-44d5-b389-8f6846bcf809'
+            }
+          }
+      ],
+      'priceMode': 'Platform',
+      'totalPrice': {
+          'currencyCode': 'EUR',
+          'centAmount': 76875
+      },
+      'lineItemMode': 'Standard'
     }
   ],
   'cartState': 'Active',
