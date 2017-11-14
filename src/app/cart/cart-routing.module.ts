4d95bac8d07955eefe8223e74af5adf33a2cf8a1
Sallar Kaboli <sallar.kaboli@gmail.com>
2017-11-14 16:12:51 +0200

    Improve cart functionality

--- src/app/cart/cart-routing.module.ts
+++ src/app/cart/cart-routing.module.ts
@@ -2,7 +2,7 @@ import { CartComponent } from './cart.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [{ path: 'cart/:id', component: CartComponent }];
+const routes: Routes = [{ path: 'cart', component: CartComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
