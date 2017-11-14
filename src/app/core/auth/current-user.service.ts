046db52c91a74ac96fb09329d1c6ec58fe689e40
Sallar Kaboli <sallar.kaboli@gmail.com>
2017-11-14 13:57:04 +0200

    Update localstorage retrieve method

--- src/app/core/auth/current-user.service.ts
+++ src/app/core/auth/current-user.service.ts
@@ -1,9 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Headers } from '@angular/http';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs/Observable';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
-import 'rxjs/add/operator/map';
 import { OAuthToken, Customer } from '@cxcloud/ct-types/customers';
 import { LocalStorageService, LocalStorage } from 'ngx-webstorage';
 
@@ -18,22 +16,19 @@ export class CurrentUserService {
   public customer = new BehaviorSubject<Customer>(null);
 
   constructor(private storage: LocalStorageService) {
-    this.setData(this.storage.retrieve('user'));
-    this.storage.observe('user').subscribe(data => this.setData(data));
+    const token = this.storage.retrieve('token');
+    const customer = this.storage.retrieve('customer');
+    if (token) {
+      this.token.next(token);
+    }
+    if (customer) {
+      this.customer.next(customer);
+    }
+    this.token.subscribe(change => this.storage.store('token', change));
+    this.customer.subscribe(change => this.storage.store('customer', change));
   }
 
   get isLoggedIn() {
-    return this.token.getValue() !== null;
-  }
-
-  setUser(data: UserData) {
-    this.storage.store('user', data);
-  }
-
-  private setData(data: UserData) {
-    if (data) {
-      this.token.next(data.token);
-      this.customer.next(data.customer);
-    }
+    return this.customer.getValue() !== null;
   }
 }
