dcb4128fef3f4cf1c595a26364b9fa5f39cfa583
Nadia Kolodyazhnaya <nadia.kolodyazhnaya@gmail.com>
2017-11-14 11:55:10 +0200

    Added shipping form and it is validation;

--- src/app/checkout/checkout.module.ts
+++ src/app/checkout/checkout.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { SharedModule } from '../shared/shared.module';
 import { CheckoutRoutingModule } from './checkout-routing.module';
@@ -17,6 +18,8 @@ const DECLARATIONS = [
 
 @NgModule({
   imports: [
+    FormsModule,
+    ReactiveFormsModule,
     CommonModule,
     SharedModule,
     CheckoutRoutingModule
--- src/app/checkout/shipping/shipping.component.ts
+++ src/app/checkout/shipping/shipping.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, group, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Address } from '../../types/common.model';
+
+import 'rxjs/add/operator/debounceTime';
 
 @Component({
   selector: 'app-shipping',
@@ -6,10 +10,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shipping.component.scss']
 })
 export class ShippingComponent implements OnInit {
+  addressForm: FormGroup;
+  shippingAddress: Address;
+  countryList: Array<string> = ['Finland', 'Germany', 'Russia', 'United Kingdom'];
 
-  constructor() { }
+  message: string;
 
-  ngOnInit() {
+  private validationMessages = {
+    required: '* required',
+    pattern: 'Please enter a valid email address'
+  };
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.addressForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      address1: ['', Validators.required],
+      address2: '',
+      city: ['', Validators.required],
+      postcode: ['', Validators.required],
+      country: ['', Validators.required],
+      region: '',
+      phone: '',
+      email: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+')]],
+      isOtherAddress: false
+    });
+
+    Object.keys(this.addressForm.controls).forEach(key => {
+      const control = this.addressForm.get(key);
+      control.valueChanges
+      .debounceTime(1000)
+      .subscribe(value => this.setValidationMessage(control, key));
+    });
   }
 
+  setValidationMessage(c: AbstractControl, name): void {
+    console.log(c);
+    this[`${name}Message`] = '';
+    if ((c.touched || c.dirty) && c.errors) {
+      this[`${name}Message`] = Object.keys(c.errors).map(key => this.validationMessages[key]).join(' ');
+    }
+  }
 }
