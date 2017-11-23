c76f8edbe24834a5d8895255edd26655fb712415
Sallar Kaboli <sallar.kaboli@gmail.com>
2017-11-23 16:50:46 +0200

    Add contentful to homepage (collection)

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,18 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MarkdownModule } from 'ngx-markdown';
 
 import { PagesRoutingModule } from './pages-routing.module';
 import { PageComponent } from './page/page.component';
 import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    PagesRoutingModule,
-    MarkdownModule.forRoot(),
-    SharedModule
-  ],
+  imports: [CommonModule, SharedModule, PagesRoutingModule],
   declarations: [PageComponent]
 })
 export class PagesModule {}
