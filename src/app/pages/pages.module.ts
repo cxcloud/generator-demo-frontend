d515a642de4f80a880ce352fab12bbc66ed3cbe5
Sallar Kaboli <sallar.kaboli@gmail.com>
2017-11-23 14:32:22 +0200

    Add spinner to static pages

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -5,9 +5,15 @@ import { MarkdownModule } from 'ngx-markdown';
 import { PagesRoutingModule } from './pages-routing.module';
 import { PagesService } from './pages.service';
 import { PageComponent } from './page/page.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  imports: [CommonModule, PagesRoutingModule, MarkdownModule.forRoot()],
+  imports: [
+    CommonModule,
+    PagesRoutingModule,
+    MarkdownModule.forRoot(),
+    SharedModule
+  ],
   declarations: [PageComponent],
   providers: [PagesService]
 })
