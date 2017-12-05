9c885703f9ad1dad8f87fa155781840a311c22ec
Sallar Kaboli <sallar.kaboli@gmail.com>
2017-12-05 16:41:43 +0200

    Skip cleanup

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 pwd
 npm install -g now --slient
-npm install --package-lock-only --slient
-npm run build
 now --token=$NOW_TOKEN --team=$NOW_TEAM
 now alias --token=$NOW_TOKEN --team=$NOW_TEAM
