028bb05ab9228da427678856d4b1c71581d93f86
Nadia Kolodyazhnaya <nadia.kolodyazhnaya@gmail.com>
2018-02-21 14:22:02 +0200

    Filter search results by categories;

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -10,7 +10,27 @@ export class SearchComponent implements OnInit {
   // TODO: temp data
   searchResults = SEARCH_DATA;
 
+  get searchSections() {
+    return SEARCH_DATA.reduce(
+      (arr, item) => {
+        arr.push(item.section);
+        return arr;
+      },
+      ['All']
+    );
+  }
+
   constructor() {}
 
   ngOnInit() {}
+
+  filterSearchContent(section) {
+    this.searchResults = SEARCH_DATA.filter(
+      result => result.section === section
+    );
+
+    if (section === 'All') {
+      this.searchResults = SEARCH_DATA;
+    }
+  }
 }
