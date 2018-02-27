047e49a7767ed998ba71fdded97c3c50131b9888
Nadia Kolodyazhnaya <nadia.kolodyazhnaya@gmail.com>
2018-02-27 22:48:12 +0200

    Navigate to the product page when search item is clicked;

--- src/app/search/search-popover/search-popover.component.ts
+++ src/app/search/search-popover/search-popover.component.ts
@@ -18,7 +18,7 @@ export class SearchPopoverComponent implements OnInit, OnChanges {
   @Input('searchEvent') searchEvent: string;
   isPopoverShown = false;
 
-  hits: any;
+  searchResults: any;
   defaultImage = './assets/images/comingsoon.png';
 
   constructor(private router: Router, private searchService: SearchService) {}
@@ -35,7 +35,7 @@ export class SearchPopoverComponent implements OnInit, OnChanges {
       changes.searchEvent &&
       changes.searchEvent.currentValue instanceof Event
     ) {
-      this.onSearch();
+      this.navigateToSearchPage();
     }
   }
 
@@ -47,11 +47,16 @@ export class SearchPopoverComponent implements OnInit, OnChanges {
         attributesToRetrieve: 'id,name.en,description.en,images'
       })
       .subscribe((result: any) => {
-        this.hits = result.hits;
+        this.searchResults = result.hits;
       });
   }
 
-  onSearch() {
+  navigateToSearchedItem(item) {
+    this.router.navigateByUrl(`product/${item.id}`);
+    this.isPopoverShown = false;
+  }
+
+  navigateToSearchPage() {
     this.router.navigateByUrl('search');
     this.isPopoverShown = false;
   }
