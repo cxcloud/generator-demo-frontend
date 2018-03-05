02647ed6164168e5732fb72856f69bdb739250fa
Nadia Kolodyazhnaya <nadia.kolodyazhnaya@gmail.com>
2018-03-05 16:52:48 +0200

    Update access token if current one is expired for anonymous and logged in users;

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -20,9 +20,26 @@ export class AuthService {
     private cartService: CartService,
     private router: Router
   ) {
+    this.checkAccessToken();
+  }
+
+  get currentTime() {
+    return Math.floor(new Date().getTime() / 1000);
+  }
+
+  checkAccessToken() {
     const token = this.storage.retrieve('token');
-    if (!token) {
+    const customer = this.storage.retrieve('customer');
+    const expiredAt = this.storage.retrieve('expired_at');
+
+    // anonymous user: if token is expired or token does't exist
+    if ((token && expiredAt <= this.currentTime) || !token) {
+      // Update LS values
       this.loginAnonymously();
+    }
+    // logged in user: if token is expired
+    if (customer && expiredAt <= this.currentTime) {
+      this.logout();
     }
   }
 
@@ -32,6 +49,9 @@ export class AuthService {
       .subscribe(result => {
         this.currentUserService.token.next(result.token);
         this.currentUserService.customer.next(null);
+        this.currentUserService.expired.next(
+          this.currentTime + result.token['expires_in']
+        );
       });
   }
 
@@ -39,6 +59,9 @@ export class AuthService {
     this.currentUserService.customer.next(resp.customer);
     this.currentUserService.token.next(resp.token);
     this.cartService.cart.next(resp.cart);
+    this.currentUserService.expired.next(
+      this.currentTime + resp.token['expires_in']
+    );
   }
 
   public login(username: string, password: string) {
