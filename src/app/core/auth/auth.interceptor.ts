009dd7ddcd9c4207d2749f6749f26de423e73c4c
Sallar Kaboli <sallar.kaboli@gmail.com>
2018-11-16 15:37:01 +0200

    Fix errors, remove unnecessary files

--- src/app/core/auth/auth.interceptor.ts
+++ src/app/core/auth/auth.interceptor.ts
@@ -1,6 +1,11 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest
+} from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/observable';
+import { Observable } from 'rxjs';
 import { CurrentUserService } from './current-user.service';
 
 @Injectable()
