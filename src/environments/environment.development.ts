245cf515bf4c6a451380669b766539152b44c76a
Nadia Kolodyazhnaya <nadia.kolodyazhnaya@gmail.com>
2019-10-07 21:33:52 +0300

    Update environment file;

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,17 +2,17 @@ export const environment = {
   production: false,
   siteName: 'CXCloud DEV',
   commerce: {
-    apiUrl: 'https://demo.cxcloud.com/api/commerce/v1',
+    apiUrl: '/api/commerce/v1',
     indexName: 'dev_COMMERCE'
   },
   content: {
-    apiUrl: 'https://demo.cxcloud.com/api/content/v1',
+    apiUrl: '/api/content/v1',
     indexName: 'dev_CONTENT'
   },
   auth: {
-    apiUrl: 'https://demo.cxcloud.com/api/commerce/v1/auth'
+    apiUrl: '/api/commerce/v1/auth'
   },
   search: {
-    apiUrl: 'https://demo.cxcloud.com/api/search/v1'
+    apiUrl: '/api/search/v1'
   }
 };
